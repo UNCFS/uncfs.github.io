bb77d66331141e4d3f56c0f14189f9200e2f3e99
Alessandra Gherardelli <alessandra.gherardelli@gmail.com>
2017-09-04 15:43:31 +0000

    Added tabs

--- js/init.js
+++ js/init.js
@@ -4,6 +4,7 @@
     $('.button-collapse').sideNav();
     $('.carousel').carousel();
     $(".loader").fadeOut("slow");
+    $('ul.tabs').tabs('select_tab', 'tab_id');
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
